b2ea9e58460018d3dbb0719f2a03add1699b07bd
dreiv <dreivoicu@gmail.com>
2017-10-09 20:41:31 +0300

    not really what I want.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: `
-    <p>
-      app Works!
-    </p>
-  `,
-  styles: []
+	  <object class="red" type="image/svg+xml" [data]="cleanUrl('/assets/star.svg')"></object>
+    <object class="yellow" type="image/svg+xml" [data]="cleanUrl('/assets/star.svg')"></object>`,
+  styles: [`.red > svg {stroke: red} 
+  .yellow svg {stroke: yellow}`]
 })
 export class AppComponent {
-  title = 'app';
+	constructor(private _sanitizer: DomSanitizer) {}
+
+	cleanUrl(url) {
+		return this._sanitizer.bypassSecurityTrustResourceUrl(url)
+	}
 }
