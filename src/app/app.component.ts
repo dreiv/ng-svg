f158ca8040071eaecb300523271af71d06477799
dreiv <dreivoicu@gmail.com>
2017-10-10 08:19:23 +0300

    color works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<button [inlineSVG]="'/assets/star.svg'">Button with image</button>`,
-  styles: [``]
+  styles: [`/deep/ svg {color: red;}`]
 })
 export class AppComponent {
 	constructor() {}
